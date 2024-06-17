6cbd3a890c03839c79faac06297075b1de81e8fc
Tejas <tejask2567@gmail.com>
2024-06-17 12:32:42 +0530

    Changes

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // server.js
+require("dotenv").config()
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
